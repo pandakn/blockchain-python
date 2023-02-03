37b7020317708b2f3971c22451b5ffc5a80e3a92
Panda <natthawut.klangyod@gmail.com>
2023-02-03 17:54:24 +0700

    added function for check tx and function edit_tx

--- purchase_condo.py
+++ purchase_condo.py
@@ -7,16 +7,51 @@ class Blockchain:
         self.blocks = []    # เก็บ Blocks
 
         # Create Genesis block
-        genesis = self.genesis_block = Block(index=len(self.blocks) + 1, data='Genesis Block', prev_hash='0')
+        genesis = self.genesis_block = Block(index=len(self.blocks), data='Genesis Block', prev_hash='0')
         self.blocks.append(genesis)
 
     # Create block to blockchain
     def add_block(self, data):
-        index = len(self.blocks) + 1
+        index = len(self.blocks)
         prev_hash = self.blocks[-1].hash
         block = Block(index, data, prev_hash)
 
         self.blocks.append(block)
+
+    def select_condo(self):
+        condo = Condo(condo_name='Supalai Oriental', size='40 m^2', address='Sukhumvit 39', province='Bangkok', price=5300000)
+        condo_b = Condo(condo_name='LA', size='31.5 m^2', address='Sripatum', province='Bangkok', price=4000000)
+        condo_c = Condo(condo_name='Na Reva', size='35.3 m^2', address='Charoennakhon', province='Bangkok', price=3900000)
+        condo_d = Condo(condo_name='The ESSE', size='32 m^2', address='Sukhumvit 36', province='Bangkok', price=6400000)
+        condo_e = Condo(condo_name='Impression', size='37 m^2', address='Charoennakhon', province='Bangkok', price=3700000)        
+
+        print("1. Supalai Oriental")
+        print("2. LA")
+        print("3. Na Reva")
+        print("4. The ESSE")
+        print("5. Impression")
+        choice = int(input("Select condo: "))
+        buyer = input("Type your name: ")
+
+        while True:
+            if choice == 1:
+                condo.purchase(buyer)
+                break
+            elif choice == 2:
+                condo_b.purchase(buyer)
+                break
+            elif choice == 3:
+                condo_c.purchase(buyer)
+                break
+            elif choice == 4:
+                condo_d.purchase(buyer)
+                break
+            elif choice == 5:
+                condo_e.purchase(buyer)
+                break
+            else:
+                print("Out of scope")
+                continue
 
     def display(self):
         for block in self.blocks:
@@ -26,13 +61,42 @@ class Blockchain:
             print(f'Timestamp : {block.timestamp}')
             print(f'Block Hash : {block.hash}\n')
 
+    def display_by_idx(self, idx):
+        for block in self.blocks:
+            if self.blocks.index(block) == idx:
+                print(f'Index : {idx}')
+                print(f'Previous Hash : {block.prev_hash}')
+                print(f'Data : {block.data}')
+                print(f'Timestamp : {block.timestamp}')
+                print(f'Block Hash : {block.hash}\n')
+
+    def edit_data(self, idx, new_owner):
+        for block in self.blocks:
+            if self.blocks.index(block) == idx:
+                block.data['buyer'] = new_owner
+                block.hash = block.compute_hash()       
+
+    def validate(self):
+        for i in range(1, len(self.blocks)):
+            current_block = self.blocks[i]
+            previous_block = self.blocks[i - 1]
+            if current_block.prev_hash != previous_block.hash:
+                return "Transaction has been edited!!"
+        return "No change."
+
 class Block:
     def __init__(self, index, data, prev_hash):
         self.index = index
         self.timestamp = str(datetime.now())
         self.data = data
         self.prev_hash = prev_hash
-        self.hash = hashlib.sha256(json.dumps(data).encode()).hexdigest()
+        # self.hash = hashlib.sha256(json.dumps(data).encode()).hexdigest()
+        self.hash = self.compute_hash()
+
+    def compute_hash(self):
+        tx = json.dumps(self.data)
+        block_contents = (str(tx) + str(self.prev_hash)).encode()
+        return hashlib.sha256(block_contents).hexdigest()
 
 class Condo:
     def __init__(self, condo_name, size, address, province, price):
@@ -43,6 +107,7 @@ class Condo:
         self.price = price
         self.owner = None
 
+    # ซื้อคอนโด
     def purchase(self, buyer):
         self.owner = buyer
         transaction_data = {
@@ -60,28 +125,39 @@ class Condo:
 
 blockchain = Blockchain()
 
-# Create a new condominium
-condo = Condo(condo_name='Supalai Oriental', size='40 m^2', address='Sukhumvit 39', province='Bangkok', price=5300000)
-condo_b = Condo(condo_name='LA', size='31.5 m^2', address='Sripatum', province='Bangkok', price=4000000)
-condo_c = Condo(condo_name='Na Reva', size='35.3 m^2', address='Charoennakhon', province='Bangkok', price=3900000)
-condo_d = Condo(condo_name='The ESSE', size='32 m^2', address='Sukhumvit 36', province='Bangkok', price=6400000)
-condo_e = Condo(condo_name='Impression', size='37 m^2', address='Charoennakhon', province='Bangkok', price=3700000)
-condo_f = Condo(condo_name='Belle Grand', size='31 m^2', address='Rama', province='Bangkok', price=4200000)
-condo_g = Condo(condo_name='The Base', size='39.4 m^2', address='Sukhumvit 50', province='Bangkok', price=6700000)
-condo_h = Condo(condo_name='The President', size='32.5 m^2', address='Sukhumvit', province='Bangkok', price=3400000)
-condo_i = Condo(condo_name='B-Loft Lite', size='30.5 m^2', address='Sukhumvit 107', province='Bangkok', price=7100000)
-condo_j = Condo(condo_name='Plum Condo', size='30.5 m^2', address='Rangsit', province='Bangkok', price=3300000)
+while True:
+    #แสดงฟังก์ชันต่างๆของระบบ
+    print("======================================================")
+    print("Select Option")
+    print("1. Purchase Condo")
+    print("2. Displays information in selected block condos")
+    print("3. Display blocks")
+    print("4. Edit TX")
+    print("5. Validate")
+    print("6. Exit")
+    print("======================================================")
 
-# Purchase the condominium  
-condo.purchase('John Doe')
-condo_b.purchase('Dang Guitar')
-condo_c.purchase('Lion Aon')
-condo_d.purchase('Panda KN')
-condo_e.purchase('Joom Meng')
-condo_f.purchase('Me Alia')
-condo_g.purchase('Eren Yeager')
-condo_h.purchase('Tony Smoke')
-condo_i.purchase('Jack The Ghost')
-condo_j.purchase('Mike Wazowski')
+    choice = int(input("Select Choice: "))
 
-blockchain.display()
+    if choice == 1:
+        condo_name = blockchain.select_condo()
+        print(condo_name)
+    elif choice == 2:
+        block_index = int(input("Blog you want to show: "))
+        blockchain.display_by_idx(block_index)
+        print("")
+    elif choice == 3:
+        blockchain.display()
+    elif choice == 4:
+        block_index = int(input("Type the block number you want to edit: "))
+        owner = input("Owner: ")
+        print("")
+        blockchain.edit_data(block_index, owner)
+    elif choice == 5:
+        valid = blockchain.validate()
+        print(valid)
+        
+    elif choice == 6:
+        break
+    else:
+        print("Wrong Choice, choose again")
